c422a52e1ff95b2b9a31bb5e2ebbd3d1f2bf15e3
Park SungSoo <soaptree45@gmail.com>
2020-06-24 23:20:32 +0900

    - add: 프로젝트 최초 커밋

---
+++ app/src/main/java/dev/soaptree/local/weather/WeatherListActivity.kt
@@ -0,0 +1,12 @@
+package dev.soaptree.local.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_list)
+    }
+}
